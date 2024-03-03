296902d989b5cf7db16542d2674cac9e30710cb9
Jing Qian <jqian2272@gmail.com>
2024-03-03 01:24:36 -0500

    verified drawline working

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 test:
 	g++ -std=c++17 -I/home/sap/CP/headers/ -o test.out test.cpp
 	./test.out
+	xdg-open aaa.ppm > /dev/null 2>&1
--- test.cpp
+++ test.cpp
@@ -3,7 +3,23 @@
 inline int rgb(int r, int g, int b) { return (r << 16) | (g << 8) | (b << 0); }
 int main() {
     Screen a(50, 50);
-    a.vertical_line(25, 0, 50, rgb(100, 100, 0));
-    a.horizontal_line(0, 50, 25, rgb(0, 100, 100));
+
+    // axes
+    a.vertical_line(25, 0, 50, rgb(100, 100, 100));
+    a.horizontal_line(0, 50, 25, rgb(100, 100, 100));
+
+    // diagonals
+    a.line(0, 0, 50, 50, rgb(20, 20, 20));
+    a.line(0, 50, 50, 0, rgb(20, 20, 20));
+
+    a.line(0, 10, 25, 25, rgb(100, 100, 0)); //yellow
+    a.line(25, 25, 10, 0, rgb(80, 80, 0)); //other yellow?
+    a.line(40, 0, 25, 25, rgb(80, 0, 0));  //red
+    a.line(50, 10, 25, 25, rgb(80, 40, 0)); //orange
+    a.line(25, 25, 0, 40, rgb(0, 0, 80));  // blue
+    a.line(25, 25, 10, 50, rgb(0, 80, 80)); //cyan
+    a.line(25, 25, 50, 40, rgb(40, 120, 40)); //light green
+    a.line(25, 25, 40, 50, rgb(0, 80, 0)); // dark green
     a.write_to_ppm("aaa.ppm");
+    // looks just like my graphing calculator LOL
 }
