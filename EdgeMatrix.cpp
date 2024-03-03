e7dfed1c4e413b8b1dccda06c3916d8195687062
Jing Qian <jqian2272@gmail.com>
2024-03-03 17:36:46 -0500

    pass by reference instead

--- EdgeMatrix.cpp
+++ EdgeMatrix.cpp
@@ -7,10 +7,10 @@ void EdgeMatrix::add_edge(int x1, int y1, int z1, int x2, int y2, int z2) {
 }
 
 /* draw all edges to screen */
-void EdgeMatrix::draw_edges(Screen s, int color) {
-    for (int i = 0; i < m.size(); ++i) {
+void EdgeMatrix::draw_edges(Screen& s, int color) {
+    for (int i = 0; i < m.size(); i += 2) {
         column a = m[i];
         column b = m[i + 1];
-        s.line(a[0], b[0], a[1], b[1], color);
+        s.line(a[0], a[1], b[0], b[1], color);
     }
 }
--- EdgeMatrix.h
+++ EdgeMatrix.h
@@ -8,11 +8,12 @@ class EdgeMatrix : private Matrix {
     /* a derivative that only allows points to be added in pairs, ie an edge */ 
     public:
     using Matrix::multiply;
+    using Matrix::to_string;
 
     void add_edge(int x1, int y1, int z1, int x2, int y2, int z2);
     
     /* draw all edges to screen */
-    void draw_edges(Screen s, int color);
+    void draw_edges(Screen& s, int color);
 };
 
 #endif // EDGE_MATRIX_H
